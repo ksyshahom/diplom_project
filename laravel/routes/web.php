18f805493bebce57709020c8d5792a0fd4a14262
Илюша <quize.web@gmail.com>
2022-05-10 10:53:53 +0300

    + added: laravel sync; ~improved: minor.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $pages = \App\Models\Page::all();
-    dd($pages);
     return view('welcome');
 });
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Start
+
+## Mac
+
+```bash
+docker-sync-stack start
+```
+
+# Stop
+
+## Mac
+
+```bash
+docker-sync-stack clean; docker volume prune -f
+```

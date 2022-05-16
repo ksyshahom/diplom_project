e1ecc1669a9a1efc4c0ce052be1bca71f1ef3ded
Илюша <quize.web@gmail.com>
2022-05-16 14:48:29 +0300

    Просмотр и удаление заявки.

--- laravel/app/Http/Controllers/AppController.php
+++ laravel/app/Http/Controllers/AppController.php
@@ -23,62 +23,72 @@ class AppController extends Controller
     public function send(Request $request)
     {
         $user = Auth::user();
-        $request->validate([
-            'first_name' => 'required',
-            'last_name' => 'required',
-            // ...
-            'nationality' => 'required',
-            'program_01' => 'required',
-            'program_02' => 'required',
-            'program_03' => 'required',
-            // ...
-            'diploma' => 'required_without:diploma_old',
-            'diploma_old' => 'required_without:diploma',
-            // ...
-        ]);
-        $data = $request->all();
-        //
-        if ($request->has('photo')) {
-            $data['photo'] = $request->file('photo')
-                ->store("public/users/$user->id");
-        } elseif ($request->has('photo_old')) {
-            $data['photo'] = $request->photo_old;
+        if (is_null($user->app) || $user->app->verified == 2) {
+//        if (true) {
+            $request->validate([
+                'first_name' => 'required',
+                'last_name' => 'required',
+                // ...
+                'nationality' => 'required',
+                'program_01' => 'required',
+                'program_02' => 'required',
+                'program_03' => 'required',
+                // ...
+                'diploma' => 'required_without:diploma_old',
+                'diploma_old' => 'required_without:diploma',
+                // ...
+            ]);
+            $data = $request->all();
+            //
+            if ($request->has('photo')) {
+                $data['photo'] = $request->file('photo')
+                    ->store("public/users/$user->id");
+            } elseif ($request->has('photo_old')) {
+                $data['photo'] = $request->photo_old;
+            }
+            //
+            if ($request->has('diploma')) {
+                $data['diploma'] = $request->file('diploma')
+                    ->store("public/users/$user->id");
+            } elseif ($request->has('diploma_old')) {
+                $data['diploma'] = $request->diploma_old;
+            }
+            //
+            $application = Application::updateOrCreate(
+                ['user_id' => $user->id],
+                ['data' => $data, 'verified' => 0]
+            );
+            //
+            DB::table('application_program')->where('application_id', $application->id)->delete();
+            DB::table('application_program')->insert([
+                'application_id' => $application->id,
+                'program_id' => $request->program_01,
+                'priority' => 1,
+            ]);
+            DB::table('application_program')->insert([
+                'application_id' => $application->id,
+                'program_id' => $request->program_02,
+                'priority' => 2,
+            ]);
+            DB::table('application_program')->insert([
+                'application_id' => $application->id,
+                'program_id' => $request->program_03,
+                'priority' => 3,
+            ]);
         }
-        //
-        if ($request->has('diploma')) {
-            $data['diploma'] = $request->file('diploma')
-                ->store("public/users/$user->id");
-        } elseif ($request->has('diploma_old')) {
-            $data['diploma'] = $request->diploma_old;
-        }
-        //
-        $application = Application::updateOrCreate(
-            ['user_id' => $user->id],
-            ['data' => $data, 'verified' => 0]
-        );
-        //
-        DB::table('application_program')->where('application_id', $application->id)->delete();
-        DB::table('application_program')->insert([
-            'application_id' => $application->id,
-            'program_id' => $request->program_01,
-            'priority' => 1,
-        ]);
-        DB::table('application_program')->insert([
-            'application_id' => $application->id,
-            'program_id' => $request->program_02,
-            'priority' => 2,
-        ]);
-        DB::table('application_program')->insert([
-            'application_id' => $application->id,
-            'program_id' => $request->program_03,
-            'priority' => 3,
-        ]);
         return redirect('/dashboard');
     }
 
     public function delete(Request $request)
     {
-        //
+        $user = Auth::user();
+        if ($user->app) {
+            $applicationProgramRows = DB::table('application_program')->where('application_id', $user->app->id)->get();
+            $applicationProgramIds = $applicationProgramRows->pluck('id')->toArray();
+            DB::table('interviews')->whereIn('application_program_id', $applicationProgramIds)->delete();
+            DB::table('application_program')->where('application_id', $user->app->id)->delete();
+            Application::where('id', $user->app->id)->delete();
+        }
     }
 
     public function item(Request $request, Application $application)
--- laravel/resources/views/app/index.blade.php
+++ laravel/resources/views/app/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Подача заявки</title>
+    <title>Заявка</title>
 </head>
 <body>
 @if ($errors->any())
@@ -19,103 +19,113 @@
     </section>
 @endif
 <main>
-    <p>Вернуться <a href="/dashboard">в личный кабинет</a>.</p>
-    <p>Подача заявки</p>
-    @if ($user->app && $user->app->verified == 0)
-        <p>Заявки находится на рассмотрении.</p>
-        <p><a href="/dashboard">Вернуться в личный кабинет.</a></p>
-    @elseif ($user->app && $user->app->verified == 1)
-        <p>Заявка рассмотрена. Можно <a href="/interview">записаться на собеседование</a>.</p>
+    <p><a href="/dashboard">Вернуться в личный кабинет.</a></p>
+    @if($user->app)
+        @switch ($user->app->verified)
+            @case(0)
+                <p>Статус: Заявка находится на рассмотрении.</p>
+                @break
+            @case(1)
+                <p>Статус: Заявка рассмотрена. Можно <a href="/interview">записаться на собеседование</a>.</p>
+                @break
+            @case(1)
+                <p>Статус: Заявка отклонена. Комментарий:</p>
+                <p>{!! $user->app->comment !!}</p>
+                @break
+        @endswitch
     @else
-        @if($user->app && $user->app->verified == 2)
-            <p>Заявка отклонена. Комментарий:</p>
-            <p>{!! $user->app->comment !!}</p>
-            <hr>
-        @endif
-        <form method="POST" enctype="multipart/form-data">
-            @csrf
-            <label>Имя*<br>
-                <input type="text" name="first_name" required
-                       value="{{ old('first_name') ?: ($user->app ? $user->app->data['first_name'] : $user->first_name) }}"><br>
-            </label>
-            <label>Фамилия*<br>
-                <input type="text" name="last_name" required
-                       value="{{ old('last_name') ?: ($user->app ? $user->app->data['last_name'] : $user->last_name) }}"><br>
-            </label>
-            <label>Отчество (если есть)<br>
-                <input type="text" name="middle_name"
-                       value="{{ old('middle_name') ?: ($user->app ? $user->app->data['middle_name'] : $user->middle_name) }}"><br>
-            </label>
-            <hr>
-            ...
-            <hr>
-            <label>Nationality*<br>
-                <input type="text" name="nationality" required
-                       value="{{ old('nationality') ?: ($user->app ? $user->app->data['nationality'] : '') }}"><br>
-            </label>
-            <hr>
-            <label>Desired Program of Study (first priority)*<br>
-                <select name="program_01" required>
-                    <option value>Выберите программу</option>
-                    @foreach ($programs as $program)
-                        <option {{ (old('program_01') == $program->id || ($user->app && $user->app->data['program_01'] == $program->id)) ? ' selected ' : '' }}
-                            value="{{ $program->id }}">{{ $program->name }}</option>
-                    @endforeach
-                </select><br>
-            </label>
-            <label>Desired Program of Study (second priority)*<br>
-                <select name="program_02" required>
-                    <option value>Выберите программу</option>
-                    @foreach ($programs as $program)
-                        <option {{ (old('program_02') == $program->id || ($user->app && $user->app->data['program_02'] == $program->id)) ? ' selected ' : '' }}
-                            value="{{ $program->id }}">{{ $program->name }}</option>
-                    @endforeach
-                </select><br>
-            </label>
-            <label>Desired Program of Study (third priority)*<br>
-                <select name="program_03" required>
-                    <option value>Выберите программу</option>
-                    @foreach ($programs as $program)
-                        <option
-                            {{ (old('program_03') == $program->id || ($user->app && $user->app->data['program_03'] == $program->id)) ? ' selected ' : '' }}
-                            value="{{ $program->id }}">{{ $program->name }}</option>
-                    @endforeach
-                </select><br>
-            </label>
-            <hr>
-            ...
-            <hr>
-            <label>A color passport-style photo<br>
-                @if($user->app)
-                    <div>
-                        <p>Ранее загруженное:</p>
-                        <img src="{{ \Illuminate\Support\Facades\Storage::url($user->app->data['photo']) }}" alt="" width="200">
-                        <input type="hidden" name="photo_old" value="{{ $user->app->data['photo'] }}">
-                    </div>
-                @endif
-                <input type="file" name="photo" value="{{ old('photo') }}"
-                       accept="image/jpeg,image/png"><br>
-            </label>
-            <hr>
-            ...
-            <hr>
-            <label>Bachelor or previous educational diploma scan*<br>
-                @if($user->app)
-                    <div>
-                        <p>Ранее загруженное: <a href="{{ \Illuminate\Support\Facades\Storage::url($user->app->data['diploma']) }}" target="_blank">просмотреть</a>.</p>
-                        <input type="hidden" name="diploma_old" value="{{ $user->app->data['diploma'] }}">
-                    </div>
-                @endif
-                <input type="file" name="diploma" value="{{ old('diploma') }}"
-                       accept="application/pdf"><br>
-            </label>
-            <hr>
-            ...
-            <hr>
-            <br>
-            <button>Подать заявку{{ $user->app ? ' повторно' : '' }}</button>
-        </form>
+        <p>Отправить новую заявку.</p>
     @endif
+    <p>Заявка:</p>
+    <hr>
+    <form method="POST" enctype="multipart/form-data">
+        @csrf
+        <label>Имя*<br>
+            <input type="text" name="first_name" required
+                   value="{{ old('first_name') ?: ($user->app ? $user->app->data['first_name'] : $user->first_name) }}"><br>
+        </label>
+        <label>Фамилия*<br>
+            <input type="text" name="last_name" required
+                   value="{{ old('last_name') ?: ($user->app ? $user->app->data['last_name'] : $user->last_name) }}"><br>
+        </label>
+        <label>Отчество (если есть)<br>
+            <input type="text" name="middle_name"
+                   value="{{ old('middle_name') ?: ($user->app ? $user->app->data['middle_name'] : $user->middle_name) }}"><br>
+        </label>
+        <hr>
+        ...
+        <hr>
+        <label>Nationality*<br>
+            <input type="text" name="nationality" required
+                   value="{{ old('nationality') ?: ($user->app ? $user->app->data['nationality'] : '') }}"><br>
+        </label>
+        <hr>
+        <label>Desired Program of Study (first priority)*<br>
+            <select name="program_01" required>
+                <option value>Выберите программу</option>
+                @foreach ($programs as $program)
+                    <option {{ (old('program_01') == $program->id || ($user->app && $user->app->data['program_01'] == $program->id)) ? ' selected ' : '' }}
+                        value="{{ $program->id }}">{{ $program->name }}</option>
+                @endforeach
+            </select><br>
+        </label>
+        <label>Desired Program of Study (second priority)*<br>
+            <select name="program_02" required>
+                <option value>Выберите программу</option>
+                @foreach ($programs as $program)
+                    <option {{ (old('program_02') == $program->id || ($user->app && $user->app->data['program_02'] == $program->id)) ? ' selected ' : '' }}
+                        value="{{ $program->id }}">{{ $program->name }}</option>
+                @endforeach
+            </select><br>
+        </label>
+        <label>Desired Program of Study (third priority)*<br>
+            <select name="program_03" required>
+                <option value>Выберите программу</option>
+                @foreach ($programs as $program)
+                    <option
+                        {{ (old('program_03') == $program->id || ($user->app && $user->app->data['program_03'] == $program->id)) ? ' selected ' : '' }}
+                        value="{{ $program->id }}">{{ $program->name }}</option>
+                @endforeach
+            </select><br>
+        </label>
+        <hr>
+        ...
+        <hr>
+        <label>A color passport-style photo<br>
+            @if($user->app)
+                <div>
+                    <p>Ранее загруженное:</p>
+                    <img src="{{ \Illuminate\Support\Facades\Storage::url($user->app->data['photo']) }}" alt="" width="200">
+                    <input type="hidden" name="photo_old" value="{{ $user->app->data['photo'] }}">
+                </div>
+            @endif
+            <input type="file" name="photo" value="{{ old('photo') }}"
+                   accept="image/jpeg,image/png"><br>
+        </label>
+        <hr>
+        ...
+        <hr>
+        <label>Bachelor or previous educational diploma scan*<br>
+            @if($user->app)
+                <div>
+                    <p>Ранее загруженное: <a href="{{ \Illuminate\Support\Facades\Storage::url($user->app->data['diploma']) }}" target="_blank">просмотреть</a>.</p>
+                    <input type="hidden" name="diploma_old" value="{{ $user->app->data['diploma'] }}">
+                </div>
+            @endif
+            <input type="file" name="diploma" value="{{ old('diploma') }}"
+                   accept="application/pdf"><br>
+        </label>
+        <hr>
+        ...
+        <hr>
+        <br>
+        @if(is_null($user->app) || $user->app->verified == 2)
+            <button>Подать заявку{{ $user->app ? ' повторно' : '' }}</button>
+        @else
+            <p>Заявку подать невозможно, т.к. она принята, либо ожидает проверки.</p>
+            <p><a href="/app/delete">Удалить заявку.</a></p>
+        @endif
+    </form>
 </main>
 </body>
 </html>
